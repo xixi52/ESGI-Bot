d895d39aa3a2e972f4548b39b26906296fb0174d
Alexis Mourey <51019403+xixi52@users.noreply.github.com>
2022-09-29 13:01:35 +0200

    Change to discord V14 and for single guild

---
+++ deploy-commands.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord.js");
+const { clientId, guildId, token } = require("./config.json");
+
+const commands = [];
+const commandsPath = path.join(__dirname, "slash-commands");
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    commands.push(command.data.toJSON());
+}
+const rest = new REST({ version: "10" }).setToken(token);
+
+console.log(rest)
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then((data) => console.log(`Successfully registered ${data.length} application commands.`))
+    .catch(console.error);
--- index.js
+++ index.js
@@ -1,140 +1,68 @@
-const { Client, Collection, MessageEmbed, Intents } = require("discord.js");
-const { promisify } = require("util"),
-  fs = require("fs"),
-  readdir = promisify(fs.readdir),
-  path = require("path"),
-  mysql = require("mysql"),
-  config = require("./config.js");
+// CLASSES
+const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
+const { token } = require("./config.json");
+const fs = require("fs"),
+path = require("path");
+const chalk = require("chalk");
+const moment = require("moment");
+const timestamp = `[${moment().format("DD-MM-YYYY HH:mm:ss")}]:`;
 
-const db = mysql.createConnection({
-  host: config.db.host,
-  user: config.db.user,
-  password: config.db.password,
-  database: config.db.name,
-  charset: "utf8mb4",
+// INSTANCE
+const client = new Client({
+    partials: [
+        Partials.Channel,
+        Partials.GuildMember,
+        Partials.User
+    ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.GuildMessages
+    ],
 });
 
-// Creates new class
-class ESGI extends Client {
-  constructor(options) {
-    super(options);
-    this.config = config; // Load config file
-    this.commands = new Collection(); // Create new collection commands
-    this.aliases = new Collection(); // Create new collection aliases
-    this.wait = promisify(setTimeout); // client.wait(1000)
-    this.collectors = {};
-    this.db = db;
-    this.emotes = config.emotes;
-    this.logger = require("./utils/logger.js");
-  }
 
-  loadCommand(commandPath, commandName) {
-    try {
-      const props = new (require(`${commandPath}${path.sep}${commandName}`))(
-        this
-      );
-      this.logger.log(`Loading Command: ${props.help.name}. 👌`, "log");
-      props.conf.location = commandPath;
-      if (props.init) {
-        props.init(this);
-      }
-      if (props.help.desc) {
-        this.commands.set(props.help.name, props);
-        client.api
-        .applications("1017053256062599298")
-        .guilds("1016710510911172638")
-        .commands.post({
-          data: {
-            name: props.help.name,
-            permission: props.conf.memberPermissions,
-            description: props.help.desc,
-            options: props.conf.options
-          },
-        });
-      }
-          
-      props.conf.aliases.forEach((alias) => {
-        this.aliases.set(alias, props.help.name);
-      });
-      return false;
-    } catch (e) {
-      return `Unable to load command ${commandName} : ${e}`;
-    }
-  }
+// COMMANDES
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "slash-commands");
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
 
-  // This function is used to unload a command (you need to load them again)
-  async unloadCommand(commandPath, commandName) {
-    let command;
-    if (this.commands.has(commandName)) {
-      command = this.commands.get(commandName);
-    } else if (this.aliases.has(commandName)) {
-      command = this.commands.get(this.aliases.get(commandName));
-    }
-    if (!command) {
-      return `The command \`${commandName}\` doesn't seem to exist, nor is it an alias. Try again!`;
-    }
-    if (command.shutdown) {
-      await command.shutdown(this);
-    }
-    delete require.cache[
-      require.resolve(`${commandPath}${path.sep}${commandName}.js`)
-    ];
-    return false;
-  }
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
 }
 
-// Creates new client
-const client = new ESGI({
-  intents: [
-      Intents.FLAGS.GUILDS,
-      Intents.FLAGS.GUILD_MESSAGES
-  ]
+// EVENTS
+client.once("ready", () => {
+  console.log(timestamp + " " + chalk.black.bgGreen.bold(' LOG ') + ` Le Bot est connecté en tant que ${client.user.tag}`)
 });
 
-const init = async () => {
-  // Then we load events, which will include our message and ready event.
-  const evtFiles = await readdir("./events/");
-  client.logger.log(`Loading a total of ${evtFiles.length} events.`, "log");
-  evtFiles.forEach((file) => {
-    const eventName = file.split(".")[0];
-    client.logger.log(`Loading Event: ${eventName}`);
-    const event = new (require(`./events/${file}`))(client);
-    client.on(eventName, (...args) => event.run(...args));
-    delete require.cache[require.resolve(`./events/${file}`)];
-  });
-
-  client.login(client.config.token).catch(console.error); // Log in to the discord api
-
-    // Search for all commands
-    let directories = await readdir("./commands/");
-    client.logger.log(
-        `Loading a total of ${directories.length} categories.`,
-        "log"
-    );
-    directories.forEach(async (dir) => {
-        let commands = await readdir("./commands/" + dir + "/");
-        commands
-        .filter((cmd) => cmd.split(".").pop() === "js")
-        .forEach((cmd) => {
-            const response = client.loadCommand("./commands/" + dir, cmd);
-            if (response) {
-            client.logger.log(response, "error");
-            }
-        });
-    });
-  
-};
-
-init();
-
-// if there are errors, log them
-client
-  .on("disconnect", () => client.logger.log("Bot is disconnecting...", "warn"))
-  .on("reconnecting", () => client.logger.log("Bot reconnecting...", "log"))
-  .on("error", (e) => client.logger.log(e, "error"))
-  .on("warn", (info) => client.logger.log(info, "log"));
-
-// if there is an unhandledRejection, log them
-process.on("unhandledRejection", (err) => {
-  console.error(err);
+client.once("reconnecting", () => {
+  console.log(timestamp + " " + chalk.black.bgYellow.bold(' LOG ') + " Le Bot tente de se reconnecter!")
 });
+
+client.once("disconnect", () => {
+  console.log(timestamp + " " + chalk.black.bgYellow.bold(' LOG ') + " Le Bot est hors ligne!")
+});
+
+// INTERACTIONS
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    console.log(timestamp + " " + chalk.black.bgWhite.bold(' LOG ') + ` L'utilisateur ${interaction.user.tag} a utilisé la commande ${interaction.name} dans #${interaction.channel.name}.`)
+
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: "Une erreur est survenue!", ephemeral: true });
+    }
+});
+
+// LOGIN
+client.login(token);
